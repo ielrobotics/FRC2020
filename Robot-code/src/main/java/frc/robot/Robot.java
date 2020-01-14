546cc782fde06d0fe509525374e65d1c3499de38
ielrobotics <ielrobotikkulubu@gmail.com>
2020-01-14 14:47:49 +0300

    ilkcanpro

--- Robot-code/src/main/java/frc/robot/Robot.java
+++ Robot-code/src/main/java/frc/robot/Robot.java
@@ -45,6 +45,9 @@ public class Robot extends TimedRobot {
     }
   }
 
+
+
+  
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
    * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
