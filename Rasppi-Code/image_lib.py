3ff36829401617882e55af18218a3a429d31b383
Deniz-Mersinlioglu <denizmersinlioglu2005@gmail.com>
2020-01-14 17:57:24 +0300

    Did scaling on data from image recognition

--- Rasppi-Code/image_lib.py
+++ Rasppi-Code/image_lib.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 import sys
-
-def color_framing(frames,l_h,l_s,l_v,u_h,u_s,u_v):
+cap = cv2.VideoCapture(0)
+def _color_framing(frames,l_h,l_s,l_v,u_h,u_s,u_v):
     frame=frames
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower=np.array([l_h,l_s,l_v])
@@ -12,11 +12,16 @@ def color_framing(frames,l_h,l_s,l_v,u_h,u_s,u_v):
     kernel = np.ones((5,5), np.uint8) 
     frame=cv2.erode(frame, kernel, iterations=1)
     return frame
-def detectcircle(frames,l_h,l_s,l_v,u_h,u_s,u_v,d_p,m_d):
-    frame=color_framing(frames,l_h,l_s,l_v,u_h,u_s,u_v)
+
+def detectcircle(l_h,l_s,l_v,u_h,u_s,u_v,d_p,m_d):
+    global cap
+    _, frames = cap.read()
+    frame=_color_framing(frames,l_h,l_s,l_v,u_h,u_s,u_v)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray,(5,5),0)
     _, thresh_img = cv2.threshold(blur,91,255,cv2.THRESH_BINARY)
+    h, w = thresh_img.shape
     circles = cv2.HoughCircles(thresh_img, cv2.HOUGH_GRADIENT, d_p, m_d)
     #each circle is (x, y, r)
-    return circles
+    #return format is [[circle, circle, ...], [w, h]]
+    return [circles, [w, h]]
--- Rasppi-Code/main.py
+++ Rasppi-Code/main.py
@@ -3,27 +3,45 @@ from networktables import NetworkTables
 import threading
 from networktables import NetworkTables
 #init stuff
-cond = threading.Condition()
-notified = [False]
+#cond = threading.Condition()
+#notified = [False]
 
-def connectionListener(connected, info):
-    global cond
-    print(info, '; Connected=%s' % connected)
-    with cond:
-        notified[0] = True
-        cond.notify()
+#def connectionListener(connected, info):
+#    global cond
+#    print(info, '; Connected=%s' % connected)
+#    with cond:
+#        notified[0] = True
+#        cond.notify()
 
-NetworkTables.initialize(server='10.80.58.2')
-NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+#NetworkTables.initialize(server='10.80.58.2')
+#NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
-with cond:
-    print("Waiting")
-    if not notified[0]:
-        cond.wait()
+#with cond:
+#    print("Waiting")
+#    if not notified[0]:
+#        cond.wait()
 
-def wrap_entry(table: nt.NetworkTable, name: str) -> nt.NetworkTableEntry:
-    return table.getEntry(name)
-instance = nt.NetworkTablesInstance.getDefault()
-table = nt.NetworkTablesInstance.getTable("datatable")
-command_pipe = wrap_entry(table, "image-processing-commands")
+#def wrap_entry(table: nt.NetworkTable, name: str) -> nt.NetworkTableEntry:
+#    return table.getEntry(name)
+#instance = nt.NetworkTablesInstance.getDefault()
+#table = nt.NetworkTablesInstance.getTable("datatable")
+#command_pipe = wrap_entry(table, "image-processing-commands")
 
+import image_lib
+while (1):
+    out = image_lib.detectcircle(13,0,0,39,255,255,3,160)
+    if out[0].__class__ == None.__class__:
+        continue
+    out[0] = out[0][0]
+    if out[0].__class__ == None.__class__:
+        continue
+    if out[0].__class__ == None.__class__:
+        continue
+    if len(out[0]) != 1:
+        continue
+    ball = out[0][0]
+    ball_x = ball[0]
+    ball_y = ball[1]
+    x = ball_x / out[1][0]
+    y = ball_y / out[1][1]
+    print([x, y])
