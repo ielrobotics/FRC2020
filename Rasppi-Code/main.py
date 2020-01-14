061694e71673d50971277f323deeeeb8b524216c
Deniz-Mersinlioglu <denizmersinlioglu2005@gmail.com>
2020-01-14 17:09:10 +0300

    added some networktables stuff

--- Rasppi-Code/main.py
+++ Rasppi-Code/main.py
@@ -1,4 +1,28 @@
 import networktables as nt
 from networktables import NetworkTables
-NetworkTables.initialize(server='roboRIO-8058-FRC.local')
-instance = NetworkTables.getTable("datatables")
+import threading
+from networktables import NetworkTables
+
+cond = threading.Condition()
+notified = [False]
+
+def connectionListener(connected, info):
+    global cond
+    print(info, '; Connected=%s' % connected)
+    with cond:
+        notified[0] = True
+        cond.notify()
+
+NetworkTables.initialize(server='10.80.58.2')
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+with cond:
+    print("Waiting")
+    if not notified[0]:
+        cond.wait()
+
+def wrap_entry(table: nt.NetworkTable, name: str) -> nt.NetworkTableEntry:
+    return table.getEntry(name)
+instance = nt.NetworkTablesInstance.getDefault()
+table = nt.NetworkTablesInstance.getTable("datatable")
+command_pipe = wrap_entry(table, "image-processing-commands")
