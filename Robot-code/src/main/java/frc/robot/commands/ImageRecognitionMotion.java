5dc1372f5d566df48928e5ea92e0075416ecf6c5
bash-the-trash <denizmersinlioglu2005@gmail.com>
2020-01-15 14:19:04 +0300

    Implemented robot side autonomous

--- Robot-code/src/main/java/frc/robot/commands/ImageRecognitionMotion.java
+++ Robot-code/src/main/java/frc/robot/commands/ImageRecognitionMotion.java
@@ -36,12 +36,24 @@ public class ImageRecognitionMotion extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double ret[] = m_comms.getXYBall();
-    if (ret.length == 3) {
-      m_chassis.drive.arcadeDrive(ret[0], ret[1]);
+    if (m_ball.slotFull()) {
+      double ret[] = m_comms.getXYHex();
+      if (ret.length == 3) {
+        m_chassis.drive.arcadeDrive(ret[0], ret[1]);
+      } else {
+        //look around
+        m_chassis.drive.arcadeDrive(0, -1);
+      }
     } else {
-      m_chassis.drive.arcadeDrive(0, 0);
+      double ret[] = m_comms.getXYBall();
+      if (ret.length == 3) {
+        m_chassis.drive.arcadeDrive(ret[0], ret[1]);
+      } else {
+        //look around
+        m_chassis.drive.arcadeDrive(0, -1);
+      }
     }
+
     
   }
 
