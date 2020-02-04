0b04320b322c1cf56a8520adb24714ca7ec76bb4
bash-the-trash <denizmersinlioglu2005@gmail.com>
2020-02-04 19:00:53 +0300

    made deploy clean everything up

--- Rasppi-Code/deploy.sh
+++ Rasppi-Code/deploy.sh
@@ -3,3 +3,4 @@ cd ..
 tar -ca Rasppi-Code -f rasppi-code-image.tar.gz
 scp rasppi-code-image.tar.gz pi@10.80.58.254:/home/pi/rasppi-code-image.tar.gz
 ssh pi@10.80.58.254 "systemctl --user stop image-recognition && rm -rf /home/pi/Rasppi-Code && tar -xf rasppi-code-image.tar.gz && systemctl --user start image-recognition && rm rasppi-code-image.tar.gz"
+rm rasppi-code-image.tar.gz
