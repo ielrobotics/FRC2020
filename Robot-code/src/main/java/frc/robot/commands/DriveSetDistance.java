77080fa233e6b28914f7bfb66cf9337f88c30dad
bash-the-trash <denizmersinlioglu2005@gmail.com>
2020-02-28 16:10:28 +0300

    Implemented DriveSetDistance

--- Robot-code/src/main/java/frc/robot/commands/DriveSetDistance.java
+++ Robot-code/src/main/java/frc/robot/commands/DriveSetDistance.java
@@ -9,34 +9,47 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Chassis;
+import frc.robot.subsystems.EncoderSubsystem;
 import frc.robot.subsystems.NavX;
 
 public class DriveSetDistance extends CommandBase {
   /**
    * Creates a new DriveSetDistance.
    */
-  public DriveSetDistance(NavX navx, Chassis chassis, ) {
+  private final EncoderSubsystem m_encoder;
+  private final Chassis m_chassis;
+  private final double distance;
+  public DriveSetDistance(EncoderSubsystem encoder, Chassis chassis, double dist) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(chassis, encoder);
+    m_chassis = chassis;
+    m_encoder = encoder;
+    distance = dist;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_encoder.reset_drive_encoders();
+    m_chassis.drive.arcadeDrive(1, 0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_chassis.drive.arcadeDrive(0, 0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_encoder.get_forward_distance() > distance;
   }
 }
--- Robot-code/src/main/java/frc/robot/commands/TurnAbsoluteAngle.java
+++ Robot-code/src/main/java/frc/robot/commands/TurnAbsoluteAngle.java
@@ -41,6 +41,7 @@ public class TurnAbsoluteAngle extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_chassis.drive.arcadeDrive(0, 0);
   }
 
   // Returns true when the command should end.
