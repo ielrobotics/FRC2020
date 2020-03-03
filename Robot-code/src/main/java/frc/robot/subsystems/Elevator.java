5a332d97580663ababafed699b63d5d964c4ec12
bash-the-trash <denizmersinlioglu2005@gmail.com>
2020-03-03 14:16:38 +0300

    switched to chameleon

--- Robot-code/src/main/java/frc/robot/subsystems/Elevator.java
+++ Robot-code/src/main/java/frc/robot/subsystems/Elevator.java
@@ -11,16 +11,19 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
   /**
    * Creates a new Elevator.
    */
-  private VictorSPX hook_motor;
+  private final VictorSPX hook_motor;
+  private final SpeedControllerGroup pull_motors;
   public Elevator() {
     this.hook_motor = new WPI_VictorSPX(3);
     elev_state = elevator_states.ELEVATOR_STOPPED;
+    pull_motors = new SpeedControllerGroup(new WPI_VictorSPX(3), new WPI_VictorSPX(4));
   }
 
   @Override
@@ -29,12 +32,16 @@ public class Elevator extends SubsystemBase {
     switch (this.elev_state) {
       case ELEVATOR_STOPPED:
         this.hook_motor.set(ControlMode.PercentOutput, 0);
+        this.pull_motors.set(0);
       break;
       case ELEVATOR_DE_ESCALATE:
-        this.hook_motor.set(ControlMode.PercentOutput, -1);
+        this.hook_motor.set(ControlMode.PercentOutput, 0);
+        //TODO: check if this is backwards
+        this.pull_motors.set(1);
       break;
       case ELEVATOR_ESCALATE:
         this.hook_motor.set(ControlMode.PercentOutput, 1);
+        this.pull_motors.set(0);
       break;
     }
     // This method will be called once per scheduler run
