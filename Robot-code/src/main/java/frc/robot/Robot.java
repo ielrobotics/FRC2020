826f5aa8ad717ada071cadba5464b8684526e349
bash-the-trash <denizmersinlioglu2005@gmail.com>
2020-03-04 12:50:03 +0300

    added camera capture

--- Robot-code/src/main/java/frc/robot/Robot.java
+++ Robot-code/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -36,6 +37,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    CameraServer.getInstance().startAutomaticCapture();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     this.m_joystick = new JoystickInterface();
